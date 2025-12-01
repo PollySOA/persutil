1ca34577351db7fe5e6d1a0ba622aff5366a9094
Alej <alejandropm06@gmail.com>
2025-12-01 07:27:12 +0100

    Entrega persutil Alejandro Pavón Martínez (#9)
    * Persutil Pavon
    
    * Base de datos
    
    * Base de Datos actualizada
    
    * Ajuste de la base del fichero sql
    
    Added a comment with the author's name in the SQL file.
    
    ---------
    
    Co-authored-by: Rafael Aznar <rafaaznar@gmail.com>

--- src/main/resources/database.sql
+++ src/main/resources/database.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: database:3306
--- Tiempo de generación: 10-11-2025 a las 11:28:03
--- Versión del servidor: 8.4.5
--- Versión de PHP: 8.2.28
+-- Tiempo de generación: 25-11-2025 a las 08:54:48
+-- Versión del servidor: 8.4.6
+-- Versión de PHP: 8.2.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -112,6 +112,31 @@ ALTER TABLE `alfonso_respuesta`
 ALTER TABLE `blog`
   MODIFY `id` bigint NOT NULL AUTO_INCREMENT;
 
+-- --------------------------------------Alejandro Pavón Martínez-------------------------------------------------
+--
+-- Estructura de tabla para la tabla `recurso`
+--
+
+CREATE TABLE `recurso` (
+  `id` bigint NOT NULL,
+  `nombre` varchar(1024) CHARACTER SET utf32 COLLATE utf32_unicode_ci NOT NULL,
+  `url` varchar(255) COLLATE utf32_unicode_ci NOT NULL,
+  `fecha_creacion` datetime NOT NULL,
+  `fecha_modificacion` datetime DEFAULT NULL,
+  `publico` tinyint(1) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf32 COLLATE=utf32_unicode_ci;
+
+--
+-- Volcado de datos para la tabla `recurso`
+--
+
+INSERT INTO `recurso` (`id`, `nombre`, `url`, `fecha_creacion`, `fecha_modificacion`, `publico`) VALUES
+(1, 'Wikipedia', 'https://wikipedia.org', '2025-11-24 11:25:21', NULL, 1),
+(2, 'ChatGPT', 'https://chatgpt.com/', '2025-11-24 11:25:21', NULL, 1),
+(3, 'GitHub', 'https://github.com/', '2025-11-24 12:47:50', NULL, 1),
+(6, 'WikiRafa', 'https://wikirafa.ausiasmarch.net/', '2025-11-25 09:43:04', '2025-11-25 09:43:08', 1),
+(7, 'Stack Overflow', 'https://stackoverflow.com/', '2025-11-25 09:45:27', NULL, 1),
+(8, 'Regex Builder', 'https://regex101.com/', '2025-11-25 09:45:53', NULL, 1);
 --
 -- Estructura de tabla para la tabla `tarea`
 --
@@ -277,6 +302,9 @@ CREATE TABLE `soares` (
 --
 
 --
+-- Indices de la tabla `recurso`
+--
+ALTER TABLE `recurso`
 -- Indices de la tabla `tarea`
 --
 ALTER TABLE `tarea`
@@ -290,6 +318,10 @@ ALTER TABLE `soares`
 --
 
 --
+-- AUTO_INCREMENT de la tabla `recurso`
+--
+ALTER TABLE `recurso`
+  MODIFY `id` bigint NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;  
 -- AUTO_INCREMENT de la tabla `tarea`
 --
 ALTER TABLE `tarea`
